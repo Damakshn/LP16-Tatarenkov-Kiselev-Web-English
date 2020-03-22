252b8b6bd3f1cf5f97daa427fb978b3e8c8b698f
Maxim Tatarenkov <maxim.tat.1988@gmail.com>
2020-03-22 20:59:44 +0300

    Модель добавления текста и шаблон для добавления текста в бд

--- config.py
+++ config.py
@@ -1,18 +1,24 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config:
     DEBUG = False
     TESTING = False
     SQLALCHEMY_DATABASE_URI = "sqlite:///:memory:"
-    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'a;hsguhrpfhupaiogoiqhwerofhuapoisdhf;aisdhf;ogehiruw'
 
 
 class DevConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///../tmp/dev.db"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "dev.db")
 
 
 class ProductionConfig(Config):
     DEBUG = True
-    SQLALCHEMY_DATABASE_URI = "sqlite:///../database.db"
+    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(basedir, "database.db")
 
 
 class TestConfig(Config):
